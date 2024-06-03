af514fe92e012d6e04a7b7d20cacdb2bc4c86354
chuy-zip <andreschuy03@gmail.com>
2024-06-03 16:52:41 -0600

    grouped data for karts and gliders

--- python/processData.py
+++ python/processData.py
@@ -1,7 +1,8 @@
 #This file will be to filter and processs data of drivers, karts and gliders.
 #Shoutouts to "spinachpants" on reddit https://www.reddit.com/r/MarioKartTour/comments/11mg9t2/coverage_spreadsheet_updated_for_the_mario_tour/
 #For making a spreadsheets for coverage and more
-# I specifically used the "data" spreadhsheet an called MKT-data
+#I specifically used the "data" spreadhsheet and called MKT-data
+#I also used the inventory and coverage to make csv files for the drivers, karts and gliders
 
 import pandas as pd
 import csv
@@ -10,14 +11,8 @@ import json
 
 allDrivers = {}
 allKarts = {}
-allGlideres = {}
+allGliders = {}
 
-itemsPreffered = {
-    'drivers': {},
-    'karts': {},
-    'gliders': {}
-
-}
 
 df = pd.read_csv("MKT-DATA.csv")
 
@@ -28,12 +23,22 @@ df = pd.read_csv("MKT-DATA.csv")
 # the type of each
 
 dfDrivers =  pd.read_csv("MKT-Drivers.csv")
-print(dfDrivers.head(5))
-
 for index, row in dfDrivers.iterrows():
     allDrivers[ row["driver"] ] = { 'skill': row["skill"], 'rarity': row['rarity']}
 
 print(allDrivers['Pauline'])
+
+
+# Getting all the available karts in the game
+dfKarts =  pd.read_csv("MKT-Karts.csv")
+for index, row in dfKarts.iterrows():
+    allKarts[ row["kart"] ] = { 'skill': row["skill"], 'rarity': row['rarity']}
+
+# Getting all the available gliders in the game
+dfGliders =  pd.read_csv("MKT-Gliders.csv")
+for index, row in dfGliders.iterrows():
+    allGliders[ row["glider"] ] = { 'skill': row["skill"], 'rarity': row['rarity']}
+
 
 df_filtered = df.dropna(subset=['Top Shelf'])
 
@@ -51,9 +56,30 @@ for index, row in df_filtered.iterrows():
         # Append the 'Top Shelf' course to the favorite_courses list
         allDrivers[itemName]['favorite_courses'].append(row["Top Shelf"])
 
+    elif itemName in allKarts.keys():
+        if 'favorite_courses' not in allKarts[itemName]:
+            allKarts[itemName]['favorite_courses'] = []
+        
+        # Append the 'Top Shelf' course to the favorite_courses list
+        allKarts[itemName]['favorite_courses'].append(row["Top Shelf"])
+    
+    else:
+        if 'favorite_courses' not in allGliders[itemName]:
+            allGliders[itemName]['favorite_courses'] = []
+        
+        # Append the 'Top Shelf' course to the favorite_courses list
+        allGliders[itemName]['favorite_courses'].append(row["Top Shelf"])
+
+
 print(allDrivers['Pauline'])
 
 print(len(allDrivers['Pauline']['favorite_courses']))
 
 with open("drivers_data.json", "w") as outfile:
     json.dump(allDrivers, outfile)
+
+with open("karts_data.json", "w") as outfile:
+    json.dump(allKarts, outfile)
+
+with open("gliders_data.json", "w") as outfile:
+    json.dump(allGliders, outfile)
