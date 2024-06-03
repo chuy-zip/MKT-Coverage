4fb42106870e3c536455642fdcc9735b1c40b04f
chuy-zip <andreschuy03@gmail.com>
2024-06-03 13:37:53 -0600

    first commit

---
+++ python/processData.py
@@ -0,0 +1,5 @@
+#This file will be to filter and processs data of drivers, karts and gliders.
+#Shoutouts to "spinachpants" on reddit https://www.reddit.com/r/MarioKartTour/comments/11mg9t2/coverage_spreadsheet_updated_for_the_mario_tour/
+#For making a spreadsheets for coverage and more
+# I specifically used the "data" spreadhsheet an called MKT-data
+
