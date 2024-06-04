aaa91f982e8d7f3a39da8fe899be6898008e09c2
chuy-zip <andreschuy03@gmail.com>
2024-06-04 17:12:37 -0600

    moved files and first test for js integration

--- app/page.js
+++ app/page.js
@@ -1,7 +1,55 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import myDrivers from "@public/python/example/MyDrivers.json"
+import courses from "@public/python/courses_data.json"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [userDrivers, setUserDrivers] = useState([])
+  const [coveredCourses, setcoveredCourses] = useState([])
+  const [allCourses, setAllCourses] = useState(courses)
+
+  const findCoursesWithoutCoverage = ( allCoursesList, coveredCoursesList) => {
+
+  }
+
+  const findCoursesWithCoverage = ( drivers ) => {
+
+  }
+
+  const fetchUserDrivers = async () => {
+    try {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      let driversList = await fetch('/python/example/MyDrivers.json')
+
+      if (!driversList.ok) {
+        throw new Error('failed to fetch drivers')
+      }
+      
+      let json_drivers = await driversList.json()
+      return json_drivers
+
+    } catch (error) {
+      console.error("Error while fetching data:", error)
+    }
+  }
+
+  
+
+  useEffect( () => {
+    const fetchData = async () => {
+      const drivers = await fetchUserDrivers();
+      setUserDrivers(drivers);
+      console.log(drivers);
+      console.log(courses)
+    };
+
+    fetchData();
+    
+  }, [])
+
   return (
     <main className={styles.main}>
       <div>
