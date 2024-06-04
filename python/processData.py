64c281aaaa39cd4f7bd1fcb53e28df8958fd6e78
chuy-zip <andreschuy03@gmail.com>
2024-06-04 15:55:47 -0600

    refactor courses json

--- python/processData.py
+++ python/processData.py
@@ -16,12 +16,27 @@ def markItemOwnership(itemName, itemList, myData, index, type, owned):
             return True
     
     return False
-        
+    
+def generateJsonForList(output_directory, fileName, list):
+    if output_directory == '':
+        with open(fileName, "w") as outfile:
+            json.dump(list, outfile, indent=4)
+    else:  
+        with open(os.path.join(output_directory, fileName), "w") as outfile:
+            json.dump(list, outfile, indent=4)
+
+def populateInitialList(df, list, type, id):
+    for _, row in df.iterrows():
+        list.append({'id': id, 'name': row[type], 'type':type,'skill': row["skill"], 'rarity': row['rarity']})
+        id = id + 1
+
+    return id
+
 # Initialize dictionaries
 allDrivers = []
 allKarts = []
 allGliders = []
-allCourses = { 'courses': []}
+allCourses = []
 
 # Load data
 df = pd.read_csv("MKT-DATA.csv")
@@ -31,29 +46,17 @@ dfGliders = pd.read_csv("MKT-Gliders.csv")
 dfCourses = pd.read_csv("MKT-Courses.csv")
 
 print(dfCourses.head(5))
-print(allCourses['courses'])
+print(allCourses)
 
 id = 1
-# Populate drivers dictionary
-for _, row in dfDrivers.iterrows():
-    allDrivers.append({'id': id, 'name': row["driver"], 'type':'driver','skill': row["skill"], 'rarity': row['rarity']})
-    id = id + 1
-
-# Populate gliders dictionary
-for _, row in dfKarts.iterrows():
-    allKarts.append({'id': id, 'name': row["kart"], 'type':'kart', 'skill': row["skill"], 'rarity': row['rarity']})
-    id = id + 1
-
-# Populate karts dictionary
-for _, row in dfGliders.iterrows():
-    allGliders.append({'id': id, 'name': row["glider"], 'type':'glider', 'skill': row["skill"], 'rarity': row['rarity']})
-    id = id + 1
-
+id = populateInitialList(dfDrivers, allDrivers, 'driver', id)
+id = populateInitialList(dfKarts, allKarts, 'kart', id)
+populateInitialList(dfGliders, allGliders, 'glider', id)
 
 # Populate courses dictionary
 for _, row in dfCourses.iterrows():
-    if row['course'] not in allCourses['courses']:
-        allCourses['courses'].append(row['course'])
+    if row['course'] not in allCourses:
+        allCourses.append(row['course'])
 
 # Filter rows with non-null 'Top Shelf'
 df_filtered = df.dropna(subset=['Top Shelf'])
@@ -71,24 +74,17 @@ for _, row in df_filtered.iterrows():
 
 # Debug information
 print(allDrivers[4])
+print(allDrivers[4]['favorite_courses'])
 print(len(allDrivers[4]['favorite_courses']))
 
 # Save to JSON files
-with open("drivers_data.json", "w") as outfile:
-    json.dump(allDrivers, outfile, indent=4)
-
-with open("karts_data.json", "w") as outfile:
-    json.dump(allKarts, outfile, indent=4)
-
-with open("gliders_data.json", "w") as outfile:
-    json.dump(allGliders, outfile, indent=4)
-
-with open("courses_data.json", "w") as outfile:
-    json.dump(allCourses, outfile, indent=4)
+generateJsonForList('', "drivers_data.json", allDrivers)
+generateJsonForList('', "karts_data.json", allKarts)
+generateJsonForList('', "gliders_data.json", allGliders)
+generateJsonForList('', "courses_data.json", allCourses)
 
 ###############
 #The next code is for processing Data of an account
-
 output_directory = "example"
 os.makedirs(output_directory, exist_ok=True)
 
@@ -101,7 +97,6 @@ myData = {
 dfMydata = pd.read_csv("example/MKT-MyData.csv")
 
 for index, row in dfMydata.iterrows():
-
     itemName = row["item"]
     owned= row['owned']
 
@@ -110,13 +105,8 @@ for index, row in dfMydata.iterrows():
         if not markItemOwnership(itemName, allKarts, myData, index, 'karts', owned):
 
             markItemOwnership(itemName, allGliders, myData, index, 'gliders', owned)
-        
 
-with open(os.path.join(output_directory, "MyDrivers.json"), "w") as outfile:
-    json.dump(myData['drivers'], outfile, indent=4)
-
-with open(os.path.join(output_directory, "MyKarts.json"), "w") as outfile:
-    json.dump(myData['karts'], outfile, indent=4)
-
-with open(os.path.join(output_directory, "MyGliders.json"), "w") as outfile:
-    json.dump(myData['gliders'], outfile, indent=4)
+# Generating more json
+generateJsonForList(output_directory, "MyDrivers.json", myData['drivers'])
+generateJsonForList(output_directory, "MyKarts.json", myData['karts'])
+generateJsonForList(output_directory, "MyGliders.json", myData['gliders'])
