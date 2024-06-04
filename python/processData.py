a2daa24cd66248b557656062410675734b845ef1
chuy-zip <andreschuy03@gmail.com>
2024-06-04 15:58:51 -0600

    ordered code

--- python/processData.py
+++ python/processData.py
@@ -2,111 +2,87 @@ import pandas as pd
 import json
 import os
 
-def addFavoriteCourseToItem(itemName, itemList, favorite_course):
-    for item in itemList:
-        if item['name'] == itemName:
+def add_favorite_course_to_item(item_name, item_list, favorite_course):
+    for item in item_list:
+        if item['name'] == item_name:
             item.setdefault('favorite_courses', []).append(favorite_course)
             return True
-        
-def markItemOwnership(itemName, itemList, myData, index, type, owned):
-    for item in itemList:
-        if item["name"] == itemName:
-            Ntype = type[:-1]
-            myData[type].append( {'id': index + 1, 'name': itemName, 'type': Ntype, 'owned': owned} )
-            return True
-    
     return False
-    
-def generateJsonForList(output_directory, fileName, list):
-    if output_directory == '':
-        with open(fileName, "w") as outfile:
-            json.dump(list, outfile, indent=4)
-    else:  
-        with open(os.path.join(output_directory, fileName), "w") as outfile:
-            json.dump(list, outfile, indent=4)
 
-def populateInitialList(df, list, type, id):
+def mark_item_ownership(item_name, item_list, my_data, index, item_type, owned):
+    for item in item_list:
+        if item["name"] == item_name:
+            my_data[item_type].append({'id': index + 1, 'name': item_name, 'type': item_type[:-1], 'owned': owned})
+            return True
+    return False
+
+def generate_json_for_list(output_directory, file_name, data_list):
+    path = os.path.join(output_directory, file_name) if output_directory else file_name
+    with open(path, "w") as outfile:
+        json.dump(data_list, outfile, indent=4)
+
+def populate_initial_list(df, data_list, item_type, start_id):
     for _, row in df.iterrows():
-        list.append({'id': id, 'name': row[type], 'type':type,'skill': row["skill"], 'rarity': row['rarity']})
-        id = id + 1
+        data_list.append({'id': start_id, 'name': row[item_type], 'type': item_type, 'skill': row["skill"], 'rarity': row['rarity']})
+        start_id += 1
+    return start_id
 
-    return id
-
-# Initialize dictionaries
-allDrivers = []
-allKarts = []
-allGliders = []
-allCourses = []
+# Initialize lists
+all_drivers, all_karts, all_gliders, all_courses = [], [], [], []
 
 # Load data
 df = pd.read_csv("MKT-DATA.csv")
-dfDrivers = pd.read_csv("MKT-Drivers.csv")
-dfKarts = pd.read_csv("MKT-Karts.csv")
-dfGliders = pd.read_csv("MKT-Gliders.csv")
-dfCourses = pd.read_csv("MKT-Courses.csv")
+df_drivers = pd.read_csv("MKT-Drivers.csv")
+df_karts = pd.read_csv("MKT-Karts.csv")
+df_gliders = pd.read_csv("MKT-Gliders.csv")
+df_courses = pd.read_csv("MKT-Courses.csv")
 
-print(dfCourses.head(5))
-print(allCourses)
+# Populate initial lists
+next_id = 1
+next_id = populate_initial_list(df_drivers, all_drivers, 'driver', next_id)
+next_id = populate_initial_list(df_karts, all_karts, 'kart', next_id)
+populate_initial_list(df_gliders, all_gliders, 'glider', next_id)
 
-id = 1
-id = populateInitialList(dfDrivers, allDrivers, 'driver', id)
-id = populateInitialList(dfKarts, allKarts, 'kart', id)
-populateInitialList(dfGliders, allGliders, 'glider', id)
-
-# Populate courses dictionary
-for _, row in dfCourses.iterrows():
-    if row['course'] not in allCourses:
-        allCourses.append(row['course'])
+# Populate courses list
+all_courses = df_courses['course'].unique().tolist()
 
 # Filter rows with non-null 'Top Shelf'
 df_filtered = df.dropna(subset=['Top Shelf'])
 
-# Update dictionaries with favorite courses
+# Update items with favorite courses
 for _, row in df_filtered.iterrows():
-    itemName = row['Name']
+    item_name = row['Name']
     favorite_course = row['Top Shelf']
-    
-    if not addFavoriteCourseToItem(itemName, allDrivers, favorite_course):
-        
-        if not addFavoriteCourseToItem(itemName, allKarts,favorite_course):
-
-            addFavoriteCourseToItem(itemName, allGliders, favorite_course)
+    if not add_favorite_course_to_item(item_name, all_drivers, favorite_course):
+        if not add_favorite_course_to_item(item_name, all_karts, favorite_course):
+            add_favorite_course_to_item(item_name, all_gliders, favorite_course)
 
 # Debug information
-print(allDrivers[4])
-print(allDrivers[4]['favorite_courses'])
-print(len(allDrivers[4]['favorite_courses']))
+print(all_drivers[4])
+print(all_drivers[4]['favorite_courses'])
+print(len(all_drivers[4]['favorite_courses']))
 
 # Save to JSON files
-generateJsonForList('', "drivers_data.json", allDrivers)
-generateJsonForList('', "karts_data.json", allKarts)
-generateJsonForList('', "gliders_data.json", allGliders)
-generateJsonForList('', "courses_data.json", allCourses)
+generate_json_for_list('', "drivers_data.json", all_drivers)
+generate_json_for_list('', "karts_data.json", all_karts)
+generate_json_for_list('', "gliders_data.json", all_gliders)
+generate_json_for_list('', "courses_data.json", all_courses)
 
-###############
-#The next code is for processing Data of an account
+# Processing account data
 output_directory = "example"
 os.makedirs(output_directory, exist_ok=True)
 
-myData = {
-    'drivers' : [],
-    'karts': [],
-    'gliders': []
-}
+my_data = {'drivers': [], 'karts': [], 'gliders': []}
+df_mydata = pd.read_csv("example/MKT-MyData.csv")
 
-dfMydata = pd.read_csv("example/MKT-MyData.csv")
+for index, row in df_mydata.iterrows():
+    item_name = row["item"]
+    owned = row['owned']
+    if not mark_item_ownership(item_name, all_drivers, my_data, index, 'drivers', owned):
+        if not mark_item_ownership(item_name, all_karts, my_data, index, 'karts', owned):
+            mark_item_ownership(item_name, all_gliders, my_data, index, 'gliders', owned)
 
-for index, row in dfMydata.iterrows():
-    itemName = row["item"]
-    owned= row['owned']
-
-    if not markItemOwnership(itemName, allDrivers, myData, index, 'drivers', owned):
-
-        if not markItemOwnership(itemName, allKarts, myData, index, 'karts', owned):
-
-            markItemOwnership(itemName, allGliders, myData, index, 'gliders', owned)
-
-# Generating more json
-generateJsonForList(output_directory, "MyDrivers.json", myData['drivers'])
-generateJsonForList(output_directory, "MyKarts.json", myData['karts'])
-generateJsonForList(output_directory, "MyGliders.json", myData['gliders'])
+# Save account data to JSON files
+generate_json_for_list(output_directory, "MyDrivers.json", my_data['drivers'])
+generate_json_for_list(output_directory, "MyKarts.json", my_data['karts'])
+generate_json_for_list(output_directory, "MyGliders.json", my_data['gliders'])
