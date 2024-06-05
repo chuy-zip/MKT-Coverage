b31da48278e76d225d9f9b3b488b1f154b443d5f
chuy-zip <andreschuy03@gmail.com>
2024-06-04 18:12:45 -0600

    added function for driver coverage

--- app/page.js
+++ app/page.js
@@ -3,6 +3,8 @@
 import styles from "./page.module.css";
 import myDrivers from "@public/python/example/MyDrivers.json"
 import courses from "@public/python/courses_data.json"
+import drivers from "@public/python/drivers_data.json"
+
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -10,13 +12,33 @@ export default function Home() {
   const [userDrivers, setUserDrivers] = useState([])
   const [coveredCourses, setcoveredCourses] = useState([])
   const [allCourses, setAllCourses] = useState(courses)
+  const [allDrivers, setAllDrivers] = useState(drivers)
 
-  const findCoursesWithoutCoverage = ( allCoursesList, coveredCoursesList) => {
+  const findCoursesWithoutCoverage = (allCoursesList, coveredCoursesList) => {
 
   }
 
-  const findCoursesWithCoverage = ( drivers ) => {
+  const findCoursesWithCoverage = (allDrivers, allUserDrivers) => {
+    let coveredCourses = new Set();
 
+    //we iterate through all the drivers available
+    allUserDrivers.forEach(userDriver => {
+
+      //if the user has it
+      if (userDriver.owned) {
+
+        //Getting the driver with the corresponding id
+        const driver = allDrivers.find(d => d.id === userDriver.id);
+        // Iterate through the favorite courses and add them to the list
+        // as coveredCourses is a Set it wont accept repeated elemnts
+        if (driver) {
+          driver.favorite_courses.forEach(course => coveredCourses.add(course));
+        }
+
+      }
+    })
+    //Get the array from the set
+    return Array.from(coveredCourses);
   }
 
   const fetchUserDrivers = async () => {
@@ -27,7 +49,7 @@ export default function Home() {
       if (!driversList.ok) {
         throw new Error('failed to fetch drivers')
       }
-      
+
       let json_drivers = await driversList.json()
       return json_drivers
 
@@ -36,18 +58,19 @@ export default function Home() {
     }
   }
 
-  
 
-  useEffect( () => {
+
+  useEffect(() => {
     const fetchData = async () => {
-      const drivers = await fetchUserDrivers();
-      setUserDrivers(drivers);
-      console.log(drivers);
+      const Udrivers = await fetchUserDrivers();
+      setUserDrivers(Udrivers);
+      console.log(Udrivers);
       console.log(courses)
+      console.log(allDrivers)
+      console.log("Have coverage", findCoursesWithCoverage(allDrivers, Udrivers))
     };
 
     fetchData();
-    
   }, [])
 
   return (
